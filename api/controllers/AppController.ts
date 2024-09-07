2cc2e29043b22485def97032efe156f1bf759f55
Hoang Hy <hoanghy01122003@gmail.com>
2024-09-08 03:51:03 +0700

    fix

--- api/controllers/AppController.ts
+++ api/controllers/AppController.ts
@@ -1,7 +1,7 @@
 import AppService from "../services/AppService";
 
 export default {
-  getData: (req, res) => {
+  getData: (req: any, res: any, next: Function) => {
     return res.json(AppService.getInfo());
   },
 };
